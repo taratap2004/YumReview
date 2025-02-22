58afa45540aad0831cbed5b9240e05f44d702522
Taratap Suratman <taralnw226059@gmail.com>
2025-02-22 22:54:59 +0700

    ..

--- yumreview/lib/supabase.ts
+++ yumreview/lib/supabase.ts
@@ -1,7 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'YOUR_SUPABASE_URL'; // ใส่ URL จาก Supabase
-const supabaseKey = 'YOUR_SUPABASE_KEY'; // ใส่ Key จาก Supabase
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseUrl = 'https://rltxgwnknpnrsnzyqbps.supabase.co' // ใส่ URL จาก Supabase
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJsdHhnd25rbnBucnNuenlxYnBzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk5NTI5MTMsImV4cCI6MjA1NTUyODkxM30.AsQKMbTjsRlHyw4AEn9nE2OsiIMN8uNxIZP7nuY_bdE' // ใส่ Key จาก Supabase
+const supabase = createClient(supabaseUrl, supabaseKey)
 
-export default supabase;
+export default supabase

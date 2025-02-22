bba5f51e2af8ba3f6be6ab8488ed521f4e559d4a
Taratap Suratman <taralnw226059@gmail.com>
2025-02-22 23:07:22 +0700

    ...

--- yumreview/app/lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://rltxgwnknpnrsnzyqbps.supabase.co' // ใส่ URL จาก Supabase
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJsdHhnd25rbnBucnNuenlxYnBzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk5NTI5MTMsImV4cCI6MjA1NTUyODkxM30.AsQKMbTjsRlHyw4AEn9nE2OsiIMN8uNxIZP7nuY_bdE' // ใส่ Key จาก Supabase
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-export default supabase
